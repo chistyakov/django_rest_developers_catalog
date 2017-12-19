97961ffef8978b71a9dd5102cce4127104abf0dd
Alexander Chistyakov <al.ol.chistyakov@gmail.com>
2017-12-19 21:31:15 +0300

    improve assertions in tests for more informative error messages

--- saprun_tasks/developers/tests.py
+++ saprun_tasks/developers/tests.py
@@ -26,7 +26,7 @@ class DeveloperTestCase(TestCase):
     def test_developer_fields(self):
         response = self.client.get('/developers/1/')
 
-        self.assertEqual(
+        self.assertDictEqual(
             response.json(),
             {'name': 'Linus', 'surname': 'Torvalds',
              'skills': [], 'educations': [], 'employment_history': []}
@@ -37,7 +37,7 @@ class DeveloperTestCase(TestCase):
 
         response = self.client.get('/developers/2/')
 
-        self.assertEqual(
+        self.assertDictEqual(
             response.json(),
             {'name': 'Tflow', 'surname': None,
              'skills': [], 'educations': [], 'employment_history': []}
@@ -51,7 +51,7 @@ class DeveloperTestCase(TestCase):
 
         response = self.client.get('/developers/2/')
 
-        self.assertEqual(response.json()['skills'], ['C', 'Unix', 'Golang'])
+        self.assertListEqual(response.json()['skills'], ['C', 'Unix', 'Golang'])
 
     def test_developer_with_education(self):
         Education.objects.create(
@@ -62,7 +62,7 @@ class DeveloperTestCase(TestCase):
 
         response = self.client.get('/developers/1/')
 
-        self.assertEqual(
+        self.assertListEqual(
             response.json()['educations'],
             [{'university': {'name': 'University of Helsinki'},
               'year_of_graduation': 1996}]
@@ -129,9 +129,6 @@ class DeveloperTestCase(TestCase):
         ])
 
 
-# TODO: replace self.assertEqual with specific self.assertDictEqual, self.assertListEqual
-# for better error messages
-
 class DevelopersListTestCase(TestCase):
     def setUp(self):
         self.wozniak = Developer.objects.create(name='Stephen', surname='Wozniak')
@@ -150,7 +147,7 @@ class DevelopersListTestCase(TestCase):
     def test_developers_list_fields(self):
         response = self.client.get('/developers/')
 
-        self.assertEqual(response.json(), [
+        self.assertListEqual(response.json(), [
             {'name': 'Stephen', 'surname': 'Wozniak',
              'skills': [], 'educations': [], 'employment_history': []},
             {'name': 'Richard', 'surname': 'Stallman',
@@ -164,8 +161,8 @@ class DevelopersListTestCase(TestCase):
 
         response = self.client.get('/developers/')
 
-        self.assertEqual(response.json()[0]['skills'], [])
-        self.assertEqual(response.json()[1]['skills'], ['Unix', 'Lisp', 'Emacs'])
+        self.assertListEqual(response.json()[0]['skills'], [])
+        self.assertListEqual(response.json()[1]['skills'], ['Unix', 'Lisp', 'Emacs'])
 
     def test_developers_list_with_educations(self):
         Education.objects.create(
@@ -181,8 +178,8 @@ class DevelopersListTestCase(TestCase):
 
         response = self.client.get('/developers/')
 
-        self.assertEqual(response.json()[0]['educations'], [])
-        self.assertEqual(response.json()[1]['educations'], [
+        self.assertListEqual(response.json()[0]['educations'], [])
+        self.assertListEqual(response.json()[1]['educations'], [
             {'university': {'name': 'Harvard University'},
              'year_of_graduation': 1974},
             {'university': {'name': 'Massachusetts Institute of Technology (MIT)'},
